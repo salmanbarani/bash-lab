469c82ca8191b7acf596d84fc47954b23051676c
salmanbarani <salman.barani@postpay.io>
2023-07-30 12:58:19 +0300

    refactor

--- hackerrank/more-condition.sh
+++ hackerrank/more-condition.sh
@@ -7,7 +7,7 @@ read Z
 if [[ $X -eq $Y && $X -eq $Z ]]; then
     echo "EQUILATERAL"
 
-elif [[ (($X -eq $Y) || ($X -eq $Z)) || (($Z -eq $Y) || ($X -eq $Z)) ]]; then
+elif [[ ($X -eq $Y) || ($X -eq $Z) || ($Z -eq $Y) ]]; then
     echo "ISOSCELES"
 else
     echo "SCALENE"
