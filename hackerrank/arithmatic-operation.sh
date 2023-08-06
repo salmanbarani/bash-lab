2e6823728d19fdd9d17e8ff21cdd6f65ba36c7a5
salmanbarani <salman.barani@postpay.io>
2023-08-06 17:44:45 +0300

    arithmtic operation

--- hackerrank/arithmatic-operation.sh
+++ hackerrank/arithmatic-operation.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
+read expretion
+echo "$expretion" | bc -l | xargs printf "%.3f"
 
-# Read space-separated input and store fields in the array (IFS=Internal Field Separator)
-read -r -a operation
-
-# Perform arithmetic on the first three elements of the array and store the result in a variable
-result=$((operation[0] + operation[1] + operation[2]))
-
-# Print the result of the arithmetic operation
-echo "$result"
