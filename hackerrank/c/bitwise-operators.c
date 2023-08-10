efa6bcd4e966f19282f787c5861d95ca20877a93
salmanbarani <salman.barani@postpay.io>
2023-08-10 15:51:45 +0300

    bitwise operators

--- hackerrank/c/bitwise-operators.c
+++ hackerrank/c/bitwise-operators.c
@@ -5,22 +5,22 @@
 //Complete the following function.
 
 int max(int a, int b) {
-	if (a >= b)
-		return a;
-	return b;
+    if (a >= b)
+        return a;
+    return b;
 }
 
 void calculate_the_maximum(int n, int k) {
   //Write your code here.
   int and, or, xor; and = or = xor = 0;
   for (int i=1; i <= n-1; ++i) {
- 	for (int j= i+1; j <=n; ++j) {
-		and = (i&j) < k ? max(and, i&j)	: and;
-		or  = (i|j) < k ? max(or, i|j) : or;
-		xor = (i^j) < k ? max(xor, i^j): xor;
-	} 
+     for (int j= i+1; j <=n; ++j) {
+        and = (i&j) < k ? max(and, i&j)    : and;
+        or  = (i|j) < k ? max(or, i|j) : or;
+        xor = (i^j) < k ? max(xor, i^j): xor;
+    } 
   }
-	printf("%d\n%d\n%d\n", and, or, xor);
+    printf("%d\n%d\n%d\n", and, or, xor);
 
 }
 
