35f90d68d889e9cdcdd29cf1bbbe4ce493a347d1
salmanbarani <salman.barani@postpay.io>
2023-08-14 15:42:21 +0300

    experimenting with io

---
+++ gcc_lab/basic/io/string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+void main() {
+	int c;
+	/*
+	do {
+		c = getchar();
+		putchar(c);
+		printf(" => %d \n", c);	
+	} while( c != EOF);
+	*/
+
+	while ((c = getchar()) != EOF) {
+		putchar(c);	
+	}
+		
+	
+}
---
+++ gcc_lab/experiment/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main() {
+
+ 	char c = getchar();
+	putchar(c);
+	printf("\n");
+}
