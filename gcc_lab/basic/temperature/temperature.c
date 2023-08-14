adbb42a9207bf4f4db917a984aa9571236e9c54f
salmanbarani <salman.barani@postpay.io>
2023-08-14 14:52:45 +0300

    refactor

--- gcc_lab/basic/temperature/temperature.c
+++ gcc_lab/basic/temperature/temperature.c
@@ -12,6 +12,7 @@ void main() {
 	step = 20; // step size
 
 	fahr = lower;
+	printf("Fahr   Celsius\n");
 	while  (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);	
 		printf("%3.0f %6.1f\n", fahr, celsius);
