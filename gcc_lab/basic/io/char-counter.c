476591204a671e425172aa594b48b2d72ff11ae8
salmanbarani <salman.barani@postpay.io>
2023-08-15 14:59:28 +0300

    char counter

---
+++ gcc_lab/basic/io/char-counter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+	/*	
+		long nc;
+		nc = 0;
+
+		while (getchar() != EOF)
+			++nc;
+		printf("%ld\n", nc);
+
+	*/
+
+	double nc;
+	for (nc=0; getchar() != EOF; ++nc);
+
+	printf("%.0f\n", nc);
+}
