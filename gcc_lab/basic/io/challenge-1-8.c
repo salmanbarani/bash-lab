6841f7409ed71ea0c8f50aa2a5f3dcc076542396
salmanbarani <salman.barani@postpay.io>
2023-08-15 15:28:20 +0300

    challenge 1.8

--- gcc_lab/basic/io/challenge-1-8.c
+++ gcc_lab/basic/io/challenge-1-8.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
-#define BLANK '\s'
+#define BLANK ' '
 #define TAB   '\t'
-#define NEWLINE '\t'
+#define NEWLINE '\n'
 
 void main() { 
-	
+	int c;
+	int blank, tab, newLine;
+
+	while ((c = getchar()) != EOF ) { 
+      		if (c == BLANK)
+		       ++blank;
+		else if (c == TAB)
+			++tab;
+		else if (c == NEWLINE)
+			++newLine;		
+	}   
+
+	printf("blank:%d, tab:%d, newline:%d\n", blank, tab, newLine);	
 
 }
