291c3802b2b45319ebf0a49818d5528a48bcba03
salmanbarani <salman.barani@postpay.io>
2023-08-22 22:55:20 +0300

    better version of challenge

--- gcc_lab/basic/mathmatics/challenge-1-17.c
+++ gcc_lab/basic/mathmatics/challenge-1-17.c
@@ -3,22 +3,36 @@
 #define MAXLINECHAR 1000
 #define MAXLINE 10 // maximum lines that can be stored in an arry
 
-int getLine(int line[], lim);
+int getLine(char line[], int lim);
+
 
 int main() {
 	int len;
 	int max;	
 	char line[MAXLINECHAR];
-	char lines[MAXLINE][MAXLINECHAR];
 	max = 0;
-	i = 0;
 
-	while ((len = getLine(line, MAXLINE)) > 0 || i < 10) {
-		if (len > THRESHOLD){
-			lines[i] = line;	
-			++i;
-		}	
+	while ((len = getLine(line, MAXLINE)) > 0) {
+		if (len > THRESHOLD)
+			printf("%s\n", line);		
+		else
+			printf("less %d\n", len);
 	}
 		
 	return 0;
 } 
+
+int getLine(char s[], int lim) {
+	int c, i;
+	
+	for (i=0; i < lim-1 && (c = getchar()) != EOF && c!='\n'; ++i)
+		s[i] = c;
+	
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}	
+
+	s[i] = '\0';	
+	return i;
+}
