82650dfb69b08fc333e416254a208426d6bd1f65
salmanbarani <salman.barani@postpay.io>
2023-08-22 23:38:05 +0300

    simple version of the challenge

---
+++ gcc_lab/basic/mathmatics/challenge-1-18.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int getLine(char line[]);
+
+int main() {
+
+
+	return 0;
+}
+
+int getLine(char line[]) {
+	int i,c;	
+
+	(for i = 0; (c = getchar()) != EOF; ++i) {
+
+
+	}	
+	
+}
