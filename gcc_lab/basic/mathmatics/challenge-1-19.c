8f97fc1665b13e61da2703d5b4d597fc5cc60136
salmanbarani <salman.barani@postpay.io>
2023-08-23 23:56:21 +0300

    challenge 1 19

--- gcc_lab/basic/mathmatics/challenge-1-19.c
+++ gcc_lab/basic/mathmatics/challenge-1-19.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 
-char* reverse(char str[]);
-char* getLine();
+#define MAXLINE 200
+
+int getLine(char line[]);
+void reverse(char line[], char reversedLine[], int size);
 
 int main() {
+	int len;
+	char line[MAXLINE];
+	char reversedLine[MAXLINE];	
 	
-
-		
+	while ((len = getLine(line)) > 0) {
+		reverse(line, reversedLine, len);	
+		printf("%s\n", reversedLine);
+	}
 	return 0;
 }
 
-char* getLine():
-	int c;
 
-	while ((c = getchar()) != EOF || )
+int getLine(char line[]) {
+	int c, i;
+	for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	
+	if (c == '\n') {
+		line[i] = c;
+		++i;
+	}
+
+	line[i] = '\0';
+	return i;
+}
+
+void reverse(char line[], char r[], int size) {
+	for (int i = 0; i < size; ++i) {
+		r[i] = line[(size-1) -i];
+	} 
+}
