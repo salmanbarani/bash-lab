2ad196474ca121c5a5dd476b6c8e4c922126d7d5
salmanbarani <salman.barani@postpay.io>
2023-08-24 19:07:34 +0300

    complete challenge 1 20

--- gcc_lab/basic/scope/challenge-1-20.c
+++ gcc_lab/basic/scope/challenge-1-20.c
@@ -8,15 +8,17 @@ void detab(void);
 
 char line[MAXLINE];
 char sline[MAXLINE];
+int n = 4;
 
-int n=4;
 
 int main() {
 	int len;	
 	extern char line[];
-		
+	extern char sline[];	
+	
 	while((len = getLine()) > 0) {
-		printf("::%s", line);
+		detab();
+		printf("%s", sline); 
 	}
 
 	return 0;
@@ -41,5 +43,25 @@ int getLine() {
 }
 
 void detab() {
-	// fill this line
+	int i, j;
+	char c;
+	extern char line[];
+	extern char sline[];
+	extern int n;
+	
+	i = 0;
+	j = 0;	
+	while (c = line[i]) {
+		if (c == '\t')
+			for (int k=0; k < n; ++k){
+				sline[j] = ' ';
+				++j;	
+			}
+		else{
+			sline[j] = c;
+			++j;
+		}
+		++i;
+	}
+	
 }
