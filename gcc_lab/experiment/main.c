d9ddfae9a26dc15ebf3a42277a89f1ee69058c9c
salmanbarani <salmanAndB@outlook.com>
2023-09-19 13:32:39 +0300

    playing with constants

--- gcc_lab/experiment/main.c
+++ gcc_lab/experiment/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 char CONS = '\0';
 char NULL_CHAR = '\0';
-
+char str[] = "Salman" " " "Barani";
 
 void main() {
-	printf("%c\n", NULL_CHAR);
+	printf("str:%s , len:%ld\n", str, strlen(str));
 }
