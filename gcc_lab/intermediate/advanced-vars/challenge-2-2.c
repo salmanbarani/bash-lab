d40d1e6f45cc3acdbe9e025f77192de7e3f00983
salmanbarani <salmanAndB@outlook.com>
2023-09-19 14:09:20 +0300

    add base codes to challenge 2 2

---
+++ gcc_lab/intermediate/advanced-vars/challenge-2-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MAXLINE 100
+
+int getLine(void);
+void apply(void);
+
+char line[MAXLINE];
+char newLine[MAXLINE];
+
+int main() {
+	printf("Hello world\n");
+	return 0;
+}
