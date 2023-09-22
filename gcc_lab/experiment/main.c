8e4ae37441db8058cd76d4644464a13157b0cdcf
salmanbarani <salmanAndB@outlook.com>
2023-09-22 12:53:38 +0300

    doing experiment with conversion

--- gcc_lab/experiment/main.c
+++ gcc_lab/experiment/main.c
@@ -5,6 +5,33 @@ char CONS = '\0';
 char NULL_CHAR = '\0';
 const char c[] = "Salman";
 
+int atoi(char s[]);
+int lower(int c);
+
 void main() {
-	printf("%d\n", -2.3 % -5);
+	printf("%c\n", lower('D'));
+}
+
+int atoi(char s[])
+{
+	int i,n;
+	n = 0;
+
+	for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+	{
+		n = 10 * n + (s[i] - '0');
+		printf("RESULT:%d\n", n);
+	}
+	return n;
+}
+
+int lower(int c)
+{
+	if (c >= 'A' && c <= 'Z'){
+		printf("A:%d a:%d c:%d a-A:%d %c\n", 'A', 'a', c, 'a' - 'A', 'a' - 'A');
+		return c + 'a' - 'A';
+	}
+	else
+		return c;
+
 }
--- gcc_lab/intermediate/advanced-vars/challenge-2-2.c
+++ gcc_lab/intermediate/advanced-vars/challenge-2-2.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 
-#define MAXLINE 100
+#define MAXLINE 300
 
 int getLine(void);
-void apply(void);
 
 char line[MAXLINE];
-char newLine[MAXLINE];
+char sline[MAXLINE];
 
 int main() {
-	printf("Hello world\n");
+	
+	int len;
+	extern char line[];
+
+	while ((len = getLine()) > 0 ) {
+		printf("%s\n", line);		
+	}
 	return 0;
+
+}
+
+
+int getLine() {
+	int i,c;
+
+	extern char line[];
+
+	for (i=0; (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+
+	if (c == '\n'){
+		line[i] = c;
+		++i;
+	} 
+	
+	line[i] = '\0';
+	return i;
 }
